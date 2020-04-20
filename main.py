ab1057ea9d10154d5119a129ff088f663b893151
dickyadrian <dicky.adrian@prosa.ai>
2020-04-20 20:21:14 +0700

    Update model name

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
     if args.train:
         train('db', 51, 'output_sepconv_pytorch', 10, 1, os.path.join('Interpolation_testset', 'input'), os.path.join('Interpolation_testset', 'gt'), load_model="./converted2.pth")
     else:
-        result_file = improve_fps(1, 'test.mp4', 'result', './converted.pth')
+        result_file = improve_fps(1, 'test.mp4', 'result', './converted2.pth')
         print("Improved video stored in ", result_file)
 
 if __name__ == '__main__':
