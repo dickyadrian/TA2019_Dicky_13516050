ab7bfe7b1e030254a1554fd04f058ca4b65115a4
dickyadrian <dicky.adrian@prosa.ai>
2020-04-28 19:22:13 +0700

    Update

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-from SepConv.forward import improve_fps
-from SepConv.train import train
+from SepConv.run import improve_fps
+from DeOldify.videoColorizer import colorize_video
 import argparse
 import torch
 import os
 import warnings
 # Ignore deprecation warning thrown by pytorch
 warnings.filterwarnings("ignore", category=UserWarning) 
+torch.backends.cudnn.benchmark = True
+
 
 def main():
     parser = argparse.ArgumentParser(description="Sepconv in pytorch")
-    parser.add_argument("--train", action="store_true")
+    parser.add_argument("--colorize", action="store_true")
     args = parser.parse_args()
+    print("Is CUDA available? ", torch.cuda.is_available())
 
-    if args.train:
-        train('db', 51, 'output_sepconv_pytorch', 1000, 16, os.path.join('Interpolation_testset', 'input'), os.path.join('Interpolation_testset', 'gt'), load_model="./converted2.pth")
+    if args.colorize:
+        colorized_path = colorize_video()
+        sepconv_path = colorized_path
     else:
-        result_file = improve_fps(1, 'test.mp4', 'result', './converted2.pth')
-        print("Improved video stored in ", result_file)
+        sepconv_path = os.path.join("video", "source", "input.mp4")
+    print(colorized_path)
+    result_file = improve_fps(1, sepconv_path)
+    print("Improved video stored in ", result_file)
 
 if __name__ == '__main__':
     main()
--- test.py
+++
@@ -1,9 +0,0 @@
-from SepConv.model import SepConvNet
-import torch
-from torch.autograd import Variable
-
-if __name__ == "__main__":
-    model = SepConvNet(kernel_size=51)
-    model.epoch = Variable(torch.tensor(0, requires_grad=False))
-    model.get_kernel.load_state_dict(torch.load('./network-lf.pytorch'))
-    torch.save({'epoch': model.epoch, 'state_dict': model.state_dict(), 'kernel_size': 51}, './converted2.pth')
